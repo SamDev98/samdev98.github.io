ac15f8bc97f7f31cb8b69972d345bc84bc3f92ba
SamDev98 <rakhesambhav@gmail.com>
2021-04-21 22:17:33 +0530

    Added light mode!

--- data/scripts/main.js
+++ data/scripts/main.js
@@ -6,3 +6,17 @@ $('#mainNav a').click(function(e) {
   $('#mainNav a').removeClass('active');
   $(this).addClass('active');
 });
+
+function darkModeToggler() {
+  const themeStylesheet = document.getElementById('theme');
+  var element = document.getElementById("theme-toggler");
+  if (themeStylesheet.href.includes('light')) {
+    themeStylesheet.href = '';
+    element.classList.add("far");
+    element.classList.remove("fas");
+  } else {
+    themeStylesheet.href = 'data/css/light.css';
+    element.classList.add("fas");
+    element.classList.remove("far");
+  }
+}

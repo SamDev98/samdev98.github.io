78add99839ad445ab08ea6e0ce49c830862b7665
samdev98 <rakhesambhav@gmail.com>
2021-06-04 19:10:07 +0530

    Trying a fix

--- data/scripts/song.js
+++ data/scripts/song.js
@@ -2,7 +2,7 @@ var myAudio = document.getElementById("meditateAudio");
 var isPlaying = false;
 
 function togglePlay() {
-  window.navigator.vibrate(100);
+  navigator.vibrate(100);
   if (isPlaying) {
     document.getElementById("meditate-btn").innerHTML = "Meditate";
     myAudio.pause();
